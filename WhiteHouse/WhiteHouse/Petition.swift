b926b2a63e5fcf1b56a60532d9273e84450e8ab9
Heang Ly <heanglypa@gmail.com>
2021-08-14 20:51:41 -0400

    part 1

---
+++ WhiteHouse/WhiteHouse/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  WhiteHouse
+//
+//  Created by Heang Ly on 8/14/21.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
+
---
+++ WhiteHouse/WhiteHouse/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  WhiteHouse
+//
+//  Created by Heang Ly on 8/14/21.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
