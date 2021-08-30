0d15ada2e3776961dfdb464c678adc25a0f19ecc
Heang Ly <heanglypa@gmail.com>
2021-08-30 14:51:33 -0400

    design ui

---
+++ Instafilter/Instafilter/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by Heang Ly on 8/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var itensity: UISlider!
+    var currentImage: UIImage!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+
+    @IBAction func save(_ sender: Any) {
+    }
+
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true, completion: nil)
+        currentImage = image
+    }
+}
+
