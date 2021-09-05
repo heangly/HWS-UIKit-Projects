2cf0bbb1259dba6323a074badc6a3f6d8fa908aa
Heang Ly <heanglypa@gmail.com>
2021-09-05 12:45:08 -0400

    js

---
+++ JavascriptInjection/Extension/Action.js
@@ -0,0 +1,15 @@
+let Action = function() {}
+
+Action.prototype = {
+run: function(parameters){
+    
+},
+    
+finalize: function(parameters){
+    
+}
+};
+
+let ExtensionPreprocessingJS = new Action
+
+
---
+++ JavascriptInjection/JavascriptInjection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JavascriptInjection
+//
+//  Created by Heang Ly on 9/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
